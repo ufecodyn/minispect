cba19259b927d83de8a13493891ed880d81b65a5
vmpuri <45368418+vmpuri@users.noreply.github.com>
2020-01-21 14:43:16 -0500

    Initial commit including Arduino code and Python interface using MatPlotLib

---
+++ Spect_Interface.py
@@ -0,0 +1,69 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+import seaborn
+import serial
+import keyboard
+from time import perf_counter
+
+from matplotlib import style
+style.use('ggplot')
+
+CHANNELS = 288
+NUM_FRAMES = 100
+MAX_INTENSITY = 1024
+# x-axis: time (0:NUM_FRAMES)
+# y-axis: wavelength (1:CHANNELS)
+# z-axis: intensity (0:MAX_INTENSITY)
+
+
+spect = serial.Serial('COM3', 115200, timeout=0.1)
+snapshots = [[0]* CHANNELS] * CHANNELS
+
+def live_plot(blit):
+	x = np.linspace(0, CHANNELS, num = CHANNELS).astype(int)
+	y = np.linspace(0, NUM_FRAMES, num = NUM_FRAMES).astype(int)
+	X,Y = np.meshgrid(x,y, sparse=False)
+	fig = plt.figure()
+	ax1 = fig.add_subplot(111)
+	
+	#ax1.set_xscale(2, 'linear')
+	img = ax1.imshow(X, vmin=0, vmax=1024, interpolation = "None", cmap="inferno")
+	fig.canvas.draw()   # note that the first draw comes before setting data 
+	
+	if blit: axbackground = fig.canvas.copy_from_bbox(ax1.bbox)
+	
+	plt.show(block=False)
+	
+	while (True):
+		start = perf_counter()
+	
+		if keyboard.is_pressed('u'): 
+			spect.write('U'.encode("ascii"))
+		elif keyboard.is_pressed('i'): 
+			spect.write('L'.encode("ascii"))
+		elif keyboard.is_pressed('x'):
+			break
+			
+		spect.write('R'.encode("ascii"))
+		datareadstart = perf_counter()
+		data = (spect.readline().split())
+		datareadend = perf_counter()
+		#print(datareadend - datareadstart)
+		if(len(data) == 288):
+			data = [float(n) for n in data]
+			snapshots.insert(0, data)
+			del snapshots[-1]
+			img.set_data(snapshots)
+		if blit:
+			fig.canvas.restore_region(axbackground)
+			ax1.draw_artist(img)
+			fig.canvas.blit(ax1.bbox)
+		else: fig.canvas.draw()
+		fig.canvas.flush_events()
+		end = perf_counter()
+		#print(1.0 / (end - start), " frames per second")
+
+
+live_plot(True)
